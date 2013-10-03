4967eef97dbb21993749f95080d4f1176c11a0dd
cokeefe <chaemobile@gmail.com>
2013-10-02 23:28:41 -0700

    brightness to i

--- lib/lamp_requests.rb
+++ lib/lamp_requests.rb
@@ -12,10 +12,6 @@ module LampRequests
     update_lamp({'on' => false})
   end
 
-  # def on?
-  #   state['on']
-  # end
-
 # COLORLOOP
   def colorloop_on
     update_lamp({'effect' => 'colorloop'})
@@ -25,21 +21,13 @@ module LampRequests
     update_lamp({'effect' => 'none'})
   end
 
-  # def colorloop?
-  #   state['effect'] == 'colorloop' ? true : false
-  # end
-
 require 'pry'
 # BRIGHTNESS
   def set_brightness(args = nil)
     binding.pry
-    command = { 'bri' => args }
+    command = { 'bri' => args.to_i }
     update_lamp(command)
   end
-
-  # def state_brightness
-  #   state['bri']
-  # end
 
 # COLOR
   def set_color(args)
