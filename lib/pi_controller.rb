e8a1ed9be4a5f034c939cffcaadfa95c0e915361
cokeefe <chaemobile@gmail.com>
2013-10-03 00:42:37 -0700

    heroku

--- lib/pi_controller.rb
+++ lib/pi_controller.rb
@@ -6,12 +6,11 @@ require 'multi_json'
 module PiController
   def update
      data = get_server
-     @lamp_ip = data[0]
-     lamp_updater(@lamp_ip, data[1])
+     lamp_updater(data[0], data[1])
   end
 
   def get_server
-    uri = "http://localhost:3000/api/#{@env['TOKEN']}"
+    uri = "http://huemorme.herokuapp.com/api/#{@env['TOKEN']}"
     MultiJson.load(Net::HTTP.get(URI.parse(uri)))
   end
 
