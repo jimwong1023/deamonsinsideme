926d2408bcbac311e01f90748af920b075421c02
cokeefe <chaemobile@gmail.com>
2013-10-12 17:32:49 -0400

    find_sleep method

--- lib/pi_controller.rb
+++ lib/pi_controller.rb
@@ -5,23 +5,26 @@ require 'multi_json'
 
 module PiController
   def update
-     data = get_server
-     lamp_updater(data[0], data[1])
+     bridge_updater(lamp_data[0], lamp_data[1])
   end
 
-  def get_server
+  def lamp_data
     uri = "http://huemorme.herokuapp.com/api/#{@env['TOKEN']}"
     response = Net::HTTP.get(URI.parse(uri))
     MultiJson.load(response)
   end
-    # response.status should be 200 if everything is okay
 
-  def lamp_updater(ip, lights_commands)
+  def bridge_updater(ip, lights_commands)
     lights_commands.each do |uniq_number, command|
       lamp = Lamp.new(ip, uniq_number)
       command.each do |msg, args|
         lamp.send_command(msg, args)
       end
     end
+  end
+
+  def find_sleep
+
+    sleep(1)
   end
 end
