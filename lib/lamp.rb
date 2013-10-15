fe9c9ca13b721fca08fe7f8da58214abb0f14ad6
cokeefe <chaemobile@gmail.com>
2013-10-14 21:48:06 -0400

    lamp refactor - initialize and message for sending

--- lib/lamp.rb
+++ lib/lamp.rb
@@ -5,9 +5,14 @@ require 'multi_json'
 class Lamp
   include LampRequests
 
-  def initialize(ip, num)
+  def initialize(ip, num, commands)
     @light_number = num
     @ip = ip
+    message(commands)
+  end
+
+  def message(commands)
+    commands.each { |msg, args| send_command(msg, args) }
   end
 
   def send_command(command, args = nil)
