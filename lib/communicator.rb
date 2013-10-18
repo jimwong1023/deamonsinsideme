d7e501faea53681dadcdbcbe0c65a13b9f8036d8
cokeefe <chaemobile@gmail.com>
2013-10-18 15:44:25 -0400

    change constant var names - both used in ruby already, self

--- lib/communicator.rb
+++ lib/communicator.rb
@@ -1,41 +1,45 @@
 require 'net/http'
 require 'multi_json'
+require 'yaml'
+require 'pry'
 
-ENV = YAML::load_file(File.expand_path(File.join(__dir__, '..', 'config.yaml')))
-URI = "http://huemorme.herokuapp.com/api/#{ENV['TOKEN']}"
+API_LOCATION = YAML::load_file(File.expand_path(File.join(__dir__, '..', 'config.yaml')))
+ADDRESS = "http://huemorme.herokuapp.com/api/#{API_LOCATION['TOKEN']}"
 
 module Communicator
+  class << self
+    def huemorme_data
+      response = Net::HTTP.get(URI.parse(ADDRESS))
+      MultiJson.load(response)
+    end
 
-  def lamp_data
-    response = Net::HTTP.get(URI.parse(URI))
-    MultiJson.load(response)
-  end
+    def lights_data
+      huemorme_data[1]
+    end
 
-  def ip
-    lamp_data[0]
-  end
+    def ip
+      huemorme_data[0]
+    end
 
-  def lights_data
-    lamp_data[1]
-  end
+    def update(command)
+      binding.pry
+      address.request_put(parsed_uri.path, MultiJson.dump(command))
+    end
 
-  def update(command)
-    address.request_put(parsed_uri.path, MultiJson.dump(command))
-  end
+    def address
+      Net::HTTP.new(parsed_uri.host)
+    end
 
-  def address
-    Net::HTTP.new(parsed_uri.host)
-  end
+    def parsed_uri
+      URI.parse("#{base_uri}/state")
+    end
 
-  def parsed_uri
-    URI.parse("#{base_uri}/state")
-  end
+    def base_uri
+      "http://#{self.ip}/api/Huemormesilly/lights/#{self.lights_data}"
+    end
 
-  def base_uri
-    "http://#{ip}/api/Huemormesilly/lights/#{lights_data}"
-  end
-
-  def state
-    MultiJson.load(Net::HTTP.get(URI.parse(base_uri)))['state']
+    def state
+      MultiJson.load(Net::HTTP.get(URI.parse(base_uri)))['state']
+    end
   end
 end
