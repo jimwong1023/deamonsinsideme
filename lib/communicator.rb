a662990b24cdad96bc8926adbce4e95a4f71aff9
cokeefe <chaemobile@gmail.com>
2013-10-17 21:05:04 -0400

    reorg

--- lib/communicator.rb
+++ lib/communicator.rb
@@ -3,37 +3,37 @@ URI = "http://huemorme.herokuapp.com/api/#{ENV['TOKEN']}"
 
 module Communicator
 
-    def ip
-      lamp_data[0]
-    end
 
-    def lights_data
-      lamp_data[1]
-    end
+  def lamp_data
+    response = Net::HTTP.get(URI.parse(URI))
+    MultiJson.load(response)
+  end
 
-    def lamp_data
-      response = Net::HTTP.get(URI.parse(URI))
-      MultiJson.load(response)
-    end
+  def ip
+    lamp_data[0]
+  end
 
-    def update_lamp(command)
-      address.request_put(parsed_uri.path, MultiJson.dump(command))
-    end
+  def lights_data
+    lamp_data[1]
+  end
 
-    def address
-      Net::HTTP.new(parsed_uri.host)
-    end
+  def update_lamp(command)
+    address.request_put(parsed_uri.path, MultiJson.dump(command))
+  end
 
-    def parsed_uri
-      URI.parse("#{base_uri}/state")
-    end
+  def address
+    Net::HTTP.new(parsed_uri.host)
+  end
 
-    def base_uri
-      "http://#{ip}/api/Huemormesilly/lights/#{lights_data}"
-    end
+  def parsed_uri
+    URI.parse("#{base_uri}/state")
+  end
 
-    def state
-      MultiJson.load(Net::HTTP.get(URI.parse(base_uri)))['state']
-    end
+  def base_uri
+    "http://#{ip}/api/Huemormesilly/lights/#{lights_data}"
+  end
 
+  def state
+    MultiJson.load(Net::HTTP.get(URI.parse(base_uri)))['state']
+  end
 end
