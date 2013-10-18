253216374e3cf2808c88cfe5b81c3c10e5392a54
cokeefe <chaemobile@gmail.com>
2013-10-17 20:50:05 -0400

    re-structure for communicator client

---
+++ lib/communicator.rb
@@ -0,0 +1,39 @@
+ENV = YAML::load_file(File.expand_path(File.join(__dir__, '..', 'config.yaml')))
+URI = "http://huemorme.herokuapp.com/api/#{ENV['TOKEN']}"
+
+module Communicator
+
+    def ip
+      lamp_data[0]
+    end
+
+    def lights_data
+      lamp_data[1]
+    end
+
+    def lamp_data
+      response = Net::HTTP.get(URI.parse(URI))
+      MultiJson.load(response)
+    end
+
+    def update_lamp(command)
+      address.request_put(parsed_uri.path, MultiJson.dump(command))
+    end
+
+    def address
+      Net::HTTP.new(parsed_uri.host)
+    end
+
+    def parsed_uri
+      URI.parse("#{base_uri}/state")
+    end
+
+    def base_uri
+      "http://#{ip}/api/Huemormesilly/lights/#{lights_data}"
+    end
+
+    def state
+      MultiJson.load(Net::HTTP.get(URI.parse(base_uri)))['state']
+    end
+
+end
--- lib/lamp_controller.rb
+++ lib/lamp_controller.rb
@@ -1,25 +1,38 @@
 require_relative 'pi_controller'
+require_relative 'communicator'
 require 'yaml'
 
 class LampController
   include PiController
 
   def initialize
-    @uri = "http://huemorme.herokuapp.com/api/#{env['TOKEN']}"
-    @last_updated = lamp_data
-    @lamp_ip = ''
+    @last_updated = Communicator::lamp_data
   end
 
   def run
     loop do
-      find_app_data
+      find_app_data(Communicator::lamp_data)
       find_sleep
     end
   end
 
-  private
+  ######################
 
-  def env
-    YAML::load_file(File.expand_path(File.join(__dir__, '..', 'config.yaml')))
+  def find_sleep
+    if @last_updated == Communicator::lamp_data
+      sleep(10)
+    else
+      sleep(2)
+      @last_updated = Communicator::lamp_data
+    end
   end
+
+
+  def find_app_data(Communicator::ip, Communicator::lights_data)
+    #destroy all lamps first - clean out db
+    lights_data.each do |uniq_number, commands|
+      Lamp.new(ip, uniq_number, commands)
+    end
+  end
+
 end
